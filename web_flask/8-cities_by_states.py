63acf905e8e795e01dd34c392c13b051baadf607
dacastanogo <dcastanogomez@gmail.com>
2020-01-29 20:19:24 -0500

    Updated Task 8 and 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,26 +1,33 @@
 #!/usr/bin/python3
-"""Minimal flask app"""
-
-from flask import Flask, render_template
+""" Flask Minimal app"""
+import re
+from flask import Flask
+from flask import render_template
 from models import storage
-from models import State
+from models.state import State
+from models.city import City
+from collections import OrderedDict
+
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def closedb(foo):
-    """Closes db session"""
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
-@app.route('/cities_by_states', strict_slashes=False)
+@app.route('/states_list/', strict_slashes=False)
 def states_list():
-    """Route /cities_by_states"""
-    states = list(storage.all(State).values())
-    states.sort(key=lambda state: state.name)
-    return render_template('8-cities_by_states.html', states=states)
+    all_states = storage.all('State')
+    return render_template('7-states_list.html', states=all_states)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    all_states = storage.all('State')
+    return render_template('8-cities_by_states.html', states=all_states)
 
 
 if __name__ == '__main__':
-    storage.reload()
-    app.run("0.0.0.0", 5000)
+    app.run()
--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,36 +1,50 @@
 #!/usr/bin/python3
 """Minimal flask app"""
-
-from flask import Flask, render_template
+import re
+from flask import Flask
+from flask import render_template
 from models import storage
-from models import State
+from models.state import State
+from models.city import City
+from collections import OrderedDict
+
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def closedb(foo):
-    """Closes db session"""
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
-@app.route('/states', strict_slashes=False, defaults={'id': None})
+@app.route('/states_list/', strict_slashes=False)
+def states_list():
+    all_states = storage.all('State')
+    return render_template('7-states_list.html', states=all_states)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    all_states = storage.all('State')
+    return render_template('8-cities_by_states.html', states=all_states)
+
+
+@app.route('/states', strict_slashes=False)
+def states():
+    all_states = storage.all('State')
+    return render_template('9-states.html', states=all_states, type=0)
+
+
 @app.route('/states/<id>', strict_slashes=False)
-def states(id):
-    """Route /states"""
-    state = states = None
-    if not id:
-        states = list(storage.all(State).values())
-    else:
-        states = storage.all(State)
-        key = "State." + id
-        if key in states:
-            state = states[key]
-        else:
-            state = None
-        states = []
-    return render_template('9-states.html', **locals())
+def states_by_id(id=None):
+    x = 0
+    all_states = storage.all('State')
+    for state_id, state in all_states.items():
+        if state.id == id:
+            x = state
+            return render_template('9-states.html', states_id=x)
+    return render_template('9-states.html')
 
 
 if __name__ == '__main__':
-    storage.reload()
-    app.run("0.0.0.0", 5000)
+    app.run()
